3f6558b3b6225f07fbba0d61f5e7dd898f0f0ceb
Mario G <garcia.mga@gmail.com>
2018-11-28 12:12:39 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // PUERTO
 //==============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+//==============================
+// ENTORNO
+//==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//==============================
+// BASE DE DATOS
+//==============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:qwerty123$@ds119394.mlab.com:19394/cafe';
+}
+process.env.URLDB = urlDB;
