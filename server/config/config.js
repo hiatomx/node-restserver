1d20795678e6dca3259cc378042508313878c3e3
Mario G <garcia.mga@gmail.com>
2018-11-29 21:59:42 -0600

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,17 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 //==============================
+// TOKEN 
+//==============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.SEED = process.env.SEED || 'seed-dev';
+
+
+//==============================
 // BASE DE DATOS
 //==============================
 let urlDB;
@@ -18,6 +29,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:qwerty123$@ds119394.mlab.com:19394/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
